f70705acc920cfd773c377e92cc189def5e42138
piyush27ranjan <piyush.27ranjan@gmail.com>
2021-01-08 12:35:00 +0530

    updated event.js

--- client/event.js
+++ client/event.js
@@ -19,16 +19,33 @@
 //Note: You can use https://mlab.com/ to use store events in sandbox free version of mongodb or you can setup you own mongodb. 
 
 class Events {
+
+  constructor(){
+    this.events = new Object();
+  }
+
   // Register an event handler
   on(eventName, callback) {
-    
+    if(!this.events[eventName]){
+      this.events[eventName] = [callback];
+    } else {
+      this.events[eventName].push(callback);
+    }
   }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if(this.events[eventName]){
+      this.events[eventName].forEach(callback => {
+        callback();
+      })
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    this.events[eventName] = [];
+  }
 }
